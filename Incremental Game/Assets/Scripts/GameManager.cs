8ded1dbfda78df8c6d27565c6533450731acdb57
NifedZenaX <85544639+NifedZenaX@users.noreply.github.com>
2021-12-02 13:20:48 +0700

    Auto Collect Mechanism

--- Incremental Game/Assets/Scripts/GameManager.cs
+++ Incremental Game/Assets/Scripts/GameManager.cs
@@ -5,18 +5,79 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance = null;
+    public static GameManager Instance
     {
-        
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
+    [Range(0f, 1f)]
+    public float AutoCollectPercentage = 0.1f;
+    public ResourceConfig[] ResourcesConfigs;
+
+    public Transform ResourcesParent;
+    public ResourceController ResourcePrefab;
+
+    public Text GoldInfo;
+    public Text AutoCollectInfo;
+
+    private List<ResourceController> _activeResources = new List<ResourceController>();
+    private float _collectSecond;
+
+    private double _totalGold;
+
+    void Start()
+    {
+        AddAllResources();
+    }
+
     void Update()
     {
-        
+        _collectSecond += Time.unscaledDeltaTime;
+        if(_collectSecond >= 1f)
+        {
+            CollectPerSecond();
+            _collectSecond = 0f;
+        }
+    }
+
+    private void AddAllResources()
+    {
+        foreach (ResourceConfig config in ResourcesConfigs)
+        {
+            GameObject obj = Instantiate(ResourcePrefab.gameObject, ResourcesParent, false);
+            ResourceController resource = obj.GetComponent<ResourceController>();
+            resource.SetConfig(config);
+            _activeResources.Add(resource);
+        }
+    }
+
+    private void CollectPerSecond()
+    {
+        double output = 0f;
+        foreach (ResourceController resource in _activeResources)
+        {
+            output += resource.GetOutput();
+        }
+        output *= AutoCollectPercentage;
+        AutoCollectInfo.text = $"Auto Collect: {output.ToString("F1")}/second";
+        AddGold(output);
+    }
+
+    private void AddGold(double value)
+    {
+        _totalGold += value;
+        GoldInfo.text = $"Gold:{_totalGold.ToString("0")}";
     }
 }
+
 
 
 [System.Serializable]
