b126f498ba8118eff6b73ad3906eb9acc66ec611
NifedZenaX <85544639+NifedZenaX@users.noreply.github.com>
2021-12-05 12:38:05 +0700

    Tapping mechanism

---
+++ Incremental Game/Assets/Scripts/TapText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapText : MonoBehaviour
+{
+    public float SpawnTime = 0.5f;
+    public Text Text;
+
+    private float _spawnTIme;
+
+    private void OnEnable()
+    {
+        _spawnTIme = SpawnTime;
+    }
+
+    private void Update()
+    {
+        _spawnTIme -= Time.unscaledDeltaTime;
+        if(_spawnTIme <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Text.CrossFadeAlpha(0f, 0.5f, false);
+            if(Text.color.a == 0f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
